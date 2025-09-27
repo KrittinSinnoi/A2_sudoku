97395c5748a46ae4743d0b5accd32fb9d38ece9f
KrittinSinnoi <s6801012610057@email.kmutnb.ac.th>
2025-09-27 21:27:22 +0700

    Update suduko.js

--- suduko.js
+++ suduko.js
@@ -37,8 +37,8 @@ function setup(){
   saveBut.position((windowWidth - solveBut.width) /2.6, size + 140);
   saveBut.mousePressed(saveGame);
   
-  //let cnv = createCanvas(size,size);
-  //cnv.parent('canvas-container');
+  let cnv = createCanvas(size,size);
+  cnv.parent('canvas-container');
 
 }
 
@@ -184,4 +184,5 @@ function saveGame(){
   saveStrings(lines, 'sudoku_save.txt');
 }
   
+
 
